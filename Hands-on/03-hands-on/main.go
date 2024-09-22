9941b896a28bb004d29f6f578d350849b948a758
ArvindSingh1 <arvind_singh1@epam.com>
2024-09-22 06:02:49 +0000

    Init function

--- Hands-on/03-hands-on/main.go
+++ Hands-on/03-hands-on/main.go
@@ -5,20 +5,23 @@ import (
 	"math/rand"
 )
 
+func init() {
+	fmt.Println("This is from init function.")
+}
 func main() {
 	x := rand.Intn(350)
 	fmt.Printf("The value of x is %v\t", x)
-/*
-	if x <= 100 {
-		fmt.Println("less than 100.")
-	} else if x >= 100 && x <= 200 {
-		fmt.Println("between 101 and 200.")
-	} else if x >= 201 && x <= 250 {
-		fmt.Println("between 201 and 250.")
-	} else {
-		fmt.Println("this was more than 250.")
-	}
-*/
+	/*
+		if x <= 100 {
+			fmt.Println("less than 100.")
+		} else if x >= 100 && x <= 200 {
+			fmt.Println("between 101 and 200.")
+		} else if x >= 201 && x <= 250 {
+			fmt.Println("between 201 and 250.")
+		} else {
+			fmt.Println("this was more than 250.")
+		}
+	*/
 	switch {
 	case x <= 100:
 		fmt.Println("less than 100.")

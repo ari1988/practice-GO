0459d8af715e95408877687217a0c42825a4b388
ArvindSingh1 <arvind_singh1@epam.com>
2024-09-22 04:55:55 +0000

    01-hands-on modified

--- Hands-on/01-hands-on/main.go
+++ Hands-on/01-hands-on/main.go
@@ -1,2 +1,10 @@
 // shasum
 // shasum -a 256 ./SNOWY-EVENING.txt
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Check the shasum of txt files with txt change and without change")
+	fmt.Println("shasum -a 256 ./SNOWY-EVENING.txt")
+}
